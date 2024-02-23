6f01dcec18d51c58ecb83e60eef493a7cefc882f
Roman Danilov <romanosauce.code@gmail.com>
2024-02-24 01:14:11 +0300

    1: Add feature for display last commit of branch

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -13,3 +13,14 @@ if len(sys.argv) == 2:
             # kind, size = header.split()
         # if kind == "branch":
             # print(kind)
+else:
+    branch_path = os.path.normpath(sys.argv[1]) + "/.git/refs/heads/" +\
+            sys.argv[2]
+    with open(branch_path, "rb") as f:
+        branch_head = f.read().decode().strip()
+    commit_path = os.path.normpath(sys.argv[1]) +\
+            f"/.git/objects/{branch_head[:2]}/{branch_head[2:]}"
+    with open(commit_path, "rb") as f:
+        obj = zlib.decompress(f.read())
+        header, _, body = obj.partition(b'\x00')
+        print(body.decode().strip())
