492f572cea1d230b62e841701ffb11824f86ba13
Roman Danilov <romanosauce.code@gmail.com>
2024-02-25 00:50:14 +0300

    1: Add feature for last commit tree display

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -23,4 +23,22 @@ else:
     with open(commit_path, "rb") as f:
         obj = zlib.decompress(f.read())
         header, _, body = obj.partition(b'\x00')
-        print(body.decode().strip())
+
+    tree_id = body.split()[1].decode()
+    tree_path = os.path.normpath(sys.argv[1]) +\
+            f"/.git/objects/{tree_id[:2]}/{tree_id[2:]}"
+    with open(tree_path, "rb") as f:
+        obj = zlib.decompress(f.read())
+        header, _, body = obj.partition(b'\x00')
+    tail = body
+    while tail:
+        treeobj, _, tail = tail.partition(b'\x00')
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        num = num.hex()
+        obj_path = os.path.normpath(sys.argv[1]) +\
+                f"/.git/objects/{num[:2]}/{num[2:]}"
+        with open(obj_path, "rb") as f:
+            obj = zlib.decompress(f.read())
+            kind = obj.partition(b"\x00")[0].split()[0]
+        print(f"{kind.decode()} {num}    {tname.decode()}")
