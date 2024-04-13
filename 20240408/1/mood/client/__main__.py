d7cbe32fc89f62b7bad7459b5ad04d237a55cc9a
Roman Danilov <romanosauce.code@gmail.com>
2024-04-13 14:34:10 +0300

    Place internals of prog_client to __init__

--- 20240408/1/mood/client/__main__.py
+++ 20240408/1/mood/client/__main__.py
@@ -1,6 +1,6 @@
 """Module for starting client and print received data in parallel."""
 
-from . import prog_client
+from . import msg_reciever
 import sys
 import socket
 import threading
@@ -14,7 +14,7 @@ if len(sys.argv) == 1:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     prompt = ">>> "
     s.connect((host, port))
-    msg_handler = threading.Thread(target=prog_client.msg_reciever,
+    msg_handler = threading.Thread(target=msg_reciever,
                                    args=(s, prompt))
     msg_handler.start()
     s.sendall((sys.argv[1]+'\n').encode())
