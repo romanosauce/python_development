ed22b278bb3613887408290ab3c325af3d0a981b
Roman Danilov <romanosauce.code@gmail.com>
2024-05-05 16:41:27 +0300

    Add dodo.py for doit tasks

---
+++ 20240427/1/dodo.py
@@ -0,0 +1,28 @@
+import glob
+
+
+def task_pot():
+    return {
+            'actions': ['pybabel extract --keywords=ngettext:2,3 --keywords=_:2 mood -o server.pot'],
+            'file_dep': glob.glob('mood/server/*.py'),
+            'targets': ['server.pot'],
+            'doc': 're-create .pot',
+            }
+
+
+def task_po():
+    return {
+            'actions': ['pybabel update --ignore-pot-creation-date -D mood -i server.pot -l ru_RU.UTF-8 -d po'],
+            'file_dep': ['server.pot'],
+            'targets': ['po/ru_RU.UTF-8/LC_MESSAGES/mood.po'],
+            'doc': 'update translations',
+            }
+
+
+def task_mo():
+    return {
+            'actions': ['pybabel compile -D mood -l ru_RU.UTF-8 -d po -i po/ru_RU.UTF-8/LC_MESSAGES/mood.po'],
+            'file_dep': ['po/ru_RU.UTF-8/LC_MESSAGES/mood.po'],
+            'targets': ['po/ru_RU.UTF-8/LC_MESSAGES/mood.mo'],
+            'doc': 'compile translations',
+            }
